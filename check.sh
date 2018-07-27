b9098d13dc51471c9f3b558d9972ae057f0cf50a
lumingjie737@gmail.com <lumingjie737@gmail.com>
2018-07-27 15:31:59 +0800

    add  alarm

--- check.sh
+++ check.sh
@@ -25,3 +25,33 @@ then
 else
         log "nginx is normal "
 fi
+
+
+
+
+
+#!/bin/bash
+log(){
+        echo "[`date '+%Y-%m-%d %H:%M:%S'`] $@" >>/tmp/check.log
+}
+
+
+hostname=`hostname`
+
+text=("openssl exit with orphan rings")
+logfile="/var/log/messages"
+num=`grep -o -E "${text[0]}" $logfile|sort |uniq -c|awk '{print $1}'`
+
+if [ $num > 10 ]; then
+        curl -s $DINGDING_API -H 'Content-Type: application/json'   -d '{"msgtype": "text","text": {"content": "'$hostname' intel QAT card may crash"}}'
+        log "The intel QAT card may be crash !" 
+        log "begin to restart qat driver and nginx daemon !"
+        pkill -9 nginx
+        service qat_service restart
+        cd /usr/local/nginx/sbin/  && ./nginx &
+else 
+        log "The interl QAT card work normally."    
+fi
+
+
+
